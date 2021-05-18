12ba247c7f3c595b92d89627b3f86553b1cbe34d
Wadih Pazos <wadihjr@wpazos.com>
2021-05-18 15:03:01 -0400

    implemented admin authorization

---
+++ admin-backend/index.js
@@ -0,0 +1,115 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+const { MongoClient } = require("mongodb");
+const fetch = require("node-fetch");
+
+const DB_PASS = process.env.DB_PASS;
+
+const uri = `mongodb+srv://wadih:${DB_PASS}@clusterllb.auwn2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const CLIENT_ID = process.env.CLIENT_ID;
+const AUTHORIZATION = process.env.AUTHORIZATION;
+
+app.get("/", (req, res) => {
+  res.redirect(
+    `https://oauth2.sky.blackbaud.com/authorization?client_id=${CLIENT_ID}&response_type=code&redirect_uri=http://localhost:3000/oauth2/callback`
+  );
+});
+
+app.get("/oauth2/callback", async (req, res) => {
+  let callbackCode = req.query.code;
+  if (req.query.error) {
+    res.send(`ERROR:  ${req.query.error}`);
+  } else {
+    console.log(req.query, callbackCode);
+
+    getToken(callbackCode)
+      .then((refreshToken) => {
+        res.send(`SUCCESS:  ${refreshToken}`);
+      })
+      .catch((err) => {
+        res.send(`ERROR:  ${err.toString()}`);
+      });
+  }
+});
+
+const getToken = (callbackCode) => {
+  return new Promise((resolve, reject) => {
+    let body = {
+      grant_type: "authorization_code",
+      code: callbackCode,
+      redirect_uri: "http://localhost:3000/oauth2/callback",
+    };
+    let headers = {
+      Authorization: `Basic ${AUTHORIZATION}`,
+      "Content-Type": "application/x-www-form-urlencoded",
+    };
+    var formBody = [];
+    for (var property in body) {
+      var encodedKey = encodeURIComponent(property);
+      var encodedValue = encodeURIComponent(body[property]);
+      formBody.push(encodedKey + "=" + encodedValue);
+    }
+    formBody = formBody.join("&");
+    console.log(formBody);
+    fetch("https://oauth2.sky.blackbaud.com/token", {
+      method: "POST",
+      body: formBody,
+      headers: headers,
+    })
+      .then((res) => res.json())
+      .then(async (res) => {
+        console.log(res);
+        let refreshToken = res.refresh_token;
+        await updateToken(refreshToken)
+          .then(() => {
+            resolve(refreshToken);
+          })
+          .catch((err) => {
+            reject(err);
+          });
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
+};
+
+const updateToken = (token) => {
+  return new Promise((resolve, reject) => {
+    let database = client.db("llb");
+    let collection = database.collection("admin");
+    collection
+      .updateOne({ purpose: "oauth" }, { $set: { refresh_token: token } })
+      .then(() => {
+        resolve();
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
+};
+
+const start = async () => {
+  try {
+    client.on("connect", () => {
+      console.log("CONNECTED TO MONGODB!");
+    });
+    await client.connect();
+    app.listen(port, () => {
+      console.log(`Example app listening at http://localhost:${port}`);
+    });
+  } catch (error) {
+    await client.close();
+    console.log("APPLICATION ERROR! ", error.toString());
+  }
+};
+
+start();
